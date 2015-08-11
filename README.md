aeeb24f498782581c5728b49916f7556ba47c0a0
alex.tsymbal <alex.tsymbal@mobindustry.net>
2015-08-11 15:39:39 +0300

    add screenshot

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# emoji_keyboard
 ### Emoji keyboard with input panel
 ![Screenshot](https://github.com/frontiertsymbal/emoji_keyboard/blob/master/EmojiKeyboard.png)
 ## Usage
@@ -15,19 +14,19 @@ Create container for panel
 ```
 ### YourActivity.java
 ``` java
-private FrameLayout root_frame_layout;
-private EmojiPanel panel;
-private EmojiParser parser;
+private FrameLayout mFrameLayout;
+private EmojiPanel mPanel;
+private EmojiParser mParser;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
     setContentView(R.layout.your_activity);
 
-    root_frame_layout = (FrameLayout) findViewById(R.id.root_frame_layout);
+    mFrameLayout = (FrameLayout) findViewById(R.id.root_frame_layout);
 
 //Create new panel, set the container in which the panel will be placed and set ClickCallback to receive Spanned string with emoji and path to sticker image.
-    panel = new EmojiPanel(this, root_frame_layout, new EmojiPanel.EmojiClickCallback() {
+    mPanel = new EmojiPanel(this, mFrameLayout, new EmojiPanel.EmojiClickCallback() {
         @Override
         public void sendClicked(Spannable span) {
             //do something with received Spannable with emoji
@@ -38,25 +37,25 @@ protected void onCreate(Bundle savedInstanceState) {
         }
     });
 //Set default icons for buttons
-    panel.iconsInit();
+    mPanel.iconsInit();
 //or if you need custom icons for buttons
-    panel.iconsInit(R.drawable.ic_send_smile_levels, R.drawable.forward_blue);
+    mPanel.iconsInit(R.drawable.ic_send_smile_levels, R.drawable.forward_blue);
 
 //initialise panel                                            
-    panel.init();
+    mPanel.init();
 //if you need parse Spannable from String with emoji
-    parser = panel.getParser();
-    Spannable parsedString = parser.parse(textView.getText().toString());
+    mParser = mPanel.getParser();
+    Spannable parsedString = mParser.parse(textView.getText().toString());
 }
     @Override
     protected void onPause() {
         super.onPause();
-        panel.dissmissEmojiPopup();
+        mPanel.dissmissEmojiPopup();
     }
     @Override
     public void onBackPressed() {
-        if(panel.isEmojiAttached()) {
-            panel.dissmissEmojiPopup();
+        if(mPanel.isEmojiAttached()) {
+            mPanel.dissmissEmojiPopup();
         } else {
             super.onBackPressed();
         }
--- app/src/main/java/net/mobindustry/emoji_keyboard/MainActivity.java
+++ app/src/main/java/net/mobindustry/emoji_keyboard/MainActivity.java
@@ -13,33 +13,33 @@ import net.mobindustry.emojilib.EmojiParser;
 
 public class MainActivity extends Activity {
 
-    private TextView textView;
-    private ImageView sticker;
-    private FrameLayout root_frame_layout;
+    private TextView mTextView;
+    private ImageView mSticker;
+    private FrameLayout mFrameLayout;
 
-    private EmojiPanel panel;
-    private EmojiParser parser;
+    private EmojiPanel mPanel;
+    private EmojiParser mParser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        root_frame_layout = (FrameLayout) findViewById(R.id.root_frame_layout);
-        sticker = (ImageView) findViewById(R.id.send_sticker);
-        textView = (TextView) findViewById(R.id.send_emoji);
+        mFrameLayout = (FrameLayout) findViewById(R.id.root_frame_layout);
+        mSticker = (ImageView) findViewById(R.id.send_sticker);
+        mTextView = (TextView) findViewById(R.id.send_emoji);
 
         //Create new panel, set the container in which the panel will be placed and set
         //ClickCallback to receive Spanned string with emoji and path to sticker image.
-        panel = new EmojiPanel(this, root_frame_layout, new EmojiPanel.EmojiClickCallback() {
+        mPanel = new EmojiPanel(this, mFrameLayout, new EmojiPanel.EmojiClickCallback() {
             @Override
             public void sendClicked(Spannable span) {
-                textView.setText(span);
+                mTextView.setText(span);
             }
 
             @Override
             public void stickerClicked(String path) {
-                sticker.setImageURI(Uri.parse(path));
+                mSticker.setImageURI(Uri.parse(path));
             }
         });
 
@@ -47,26 +47,26 @@ public class MainActivity extends Activity {
         //panel.iconsInit();
 
         //or if you need custom icons for buttons
-        panel.iconsInit(R.drawable.ic_send_smile_levels, R.drawable.forward_blue);
+        mPanel.iconsInit(R.drawable.ic_send_smile_levels, R.drawable.forward_blue);
 
         //initialise panel
-        panel.init();
+        mPanel.init();
 
         //if you need parse Spannable from String with emoji
-        parser = panel.getParser();
-        Spannable parsedString = parser.parse(textView.getText().toString());
+        mParser = mPanel.getParser();
+        Spannable parsedString = mParser.parse(mTextView.getText().toString());
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        panel.dissmissEmojiPopup();
+        mPanel.dissmissEmojiPopup();
     }
 
     @Override
     public void onBackPressed() {
-        if (panel.isEmojiAttached()) {
-            panel.dissmissEmojiPopup();
+        if (mPanel.isEmojiAttached()) {
+            mPanel.dissmissEmojiPopup();
         } else {
             super.onBackPressed();
         }
